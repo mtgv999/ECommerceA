2eecc45b042982e76e6f29c97434b0db55ed10bd
mtgv999 <petermgcz@gmail.com>
2024-11-18 14:56:21 +0800

    a

---
+++ src/main/java/com/example/ecommercea/ECommerceAApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceAApplication.class, args);
+	}
+
+}
